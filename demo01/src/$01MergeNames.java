292d27504e5e1ba720a8edeaebc158244cf83760
cycyjing <cycycyjing@gmail.com>
2020-04-05 20:58:24 +1200

    demo01 TestDome Java Interview Questions 01. Merge Names way2

--- demo01/src/$01MergeNames.java
+++ demo01/src/$01MergeNames.java
@@ -1,15 +1,32 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class $01MergeNames {
 
     public static String[] uniqueNames(String[] names1, String[] names2) {
         // way 1
-        List<String> list = new ArrayList(Arrays.asList(names1));
-        list.addAll(Arrays.asList(names2));
+//        List<String> list = new ArrayList(Arrays.asList(names1));
+//        list.addAll(Arrays.asList(names2));
+//        List<String> noDupList = new ArrayList<>();
+//        list.stream().forEach(
+//                str -> {
+//                    if (!noDupList.contains(str)) {
+//                        noDupList.add(str);
+//                    }
+//                }
+//        );
+//        String[] str = new String[noDupList.size()];
+//        noDupList.toArray(str);
+
+        // way 2
+        Stream<String> n1 = Stream.of(names1);
+        Stream<String> n2 = Stream.of(names2);
+        Stream<String> names = Stream.concat(n1, n2);
+        String[] both = names.toArray(String[]::new);
         List<String> noDupList = new ArrayList<>();
-        list.stream().forEach(
+        Arrays.asList(both).stream().forEach(
                 str -> {
                     if (!noDupList.contains(str)) {
                         noDupList.add(str);
@@ -18,6 +35,7 @@ public class $01MergeNames {
         );
         String[] str = new String[noDupList.size()];
         noDupList.toArray(str);
+
 
 
         return str;
