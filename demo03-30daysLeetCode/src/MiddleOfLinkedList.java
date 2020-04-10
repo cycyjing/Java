8c6936de800f19abc2739db0392c438a61fd82b6
cycyjing <cycycyjing@gmail.com>
2020-04-10 21:43:38 +1200

    demo03 MiddleOfLinkedList finished

--- demo03-30daysLeetCode/src/MiddleOfLinkedList.java
+++ demo03-30daysLeetCode/src/MiddleOfLinkedList.java
@@ -1,3 +1,57 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
 public class MiddleOfLinkedList {
-    
+    public ListNode middleNode(ListNode head) {
+//        if (head.val < 1 || head.val > 100)
+//            return null;
+//        List<Integer> list = new LinkedList<>();
+//        if (head.next == null && (head.val < 1 || head.val > 100))
+//            return new ListNode(head.val);
+//        list.add(head.val);
+//        while (head.next != null) {
+//            head = head.next;
+//            list.add(head.val);
+//        }
+//        int size = list.size();
+//        if (size % 2 == 0) {
+//            return new ListNode(size / 2 + 1);
+//        } else if (size % 2 != 0) {
+//            return new ListNode(size / 2 + 1);
+//        } else {
+//            return null;
+//        }
+        int length = 1;
+        ListNode current = head;
+        while (current.next != null) {
+            current = current.next;
+            length++;
+        }
+
+        int middle = length / 2;
+        current = head;
+        int i = 0;
+        while (i < middle) {
+            current = current.next;
+            i++;
+        }
+        return current;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;//list
+
+    ListNode(int x) {
+        val = x;
+    }
 }
