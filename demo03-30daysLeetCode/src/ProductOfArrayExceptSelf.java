dffab84030ef523568fc0e865695ba036c83e4c7
cycyjing <cycycyjing@gmail.com>
2020-04-16 13:35:35 +1200

    demo03 ProductOfArrayExceptSelf solution2

--- demo03-30daysLeetCode/src/ProductOfArrayExceptSelf.java
+++ demo03-30daysLeetCode/src/ProductOfArrayExceptSelf.java
@@ -1,15 +1,33 @@
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         // solution 1
-        int[] output = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            int tep = 1;
-            for (int j = 0; j < nums.length; j++) {
-                if (j != i) {
-                    tep *= nums[j];
-                }
-            }
-            output[i] = tep;
+//        int[] output = new int[nums.length];
+//        for (int i = 0; i < nums.length; i++) {
+//            int tep = 1;
+//            for (int j = 0; j < nums.length; j++) {
+//                if (j != i) {
+//                    tep *= nums[j];
+//                }
+//            }
+//            output[i] = tep;
+//        }
+//        return output;
+
+        // solution 2
+        int n = nums.length;
+        int[] output = new int[n];
+        int[] forward = new int[n];
+        int[] backward = new int[n];
+        forward[0] = 1;
+        backward[n - 1] = 1;
+        for (int i = 1; i < n; i++) {
+            forward[i] = forward[i - 1] * nums[i - 1];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            backward[i] = backward[i + 1] * nums[i + 1];
+        }
+        for (int i = 0; i < n; i++) {
+            output[i] = forward[i] * backward[i];
         }
         return output;
     }
