559204d4832e1f4879ace542457fb71f892b6d3d
cycyjing <cycycyjing@gmail.com>
2020-04-24 13:46:20 +1200

    demo03 SubarraySumEqualsK solution3

--- demo03-30daysLeetCode/src/SubarraySumEqualsK.java
+++ demo03-30daysLeetCode/src/SubarraySumEqualsK.java
@@ -17,16 +17,28 @@ public class SubarraySumEqualsK {
 //        return output;
 
         // solution 2
-        List<Integer> list = new ArrayList<>();
-        list.add(0, 0);
-        int output = 0, sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            list.add(i + 1, sum);
-        }
+//        List<Integer> list = new ArrayList<>();
+//        list.add(0, 0);
+//        int output = 0, sum = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            sum += nums[i];
+//            list.add(i + 1, sum);
+//        }
+//        for (int start = 0; start < nums.length; start++) {
+//            for (int end = start + 1; end <= nums.length; end++) {
+//                if (list.get(end) - list.get(start) == k)
+//                    output++;
+//            }
+//        }
+//        return output;
+
+        // solution 3
+        int output = 0;
         for (int start = 0; start < nums.length; start++) {
-            for (int end = start + 1; end <= nums.length; end++) {
-                if (list.get(end) - list.get(start) == k)
+            int sum = 0;
+            for (int end = start; end < nums.length; end++) {
+                sum += nums[end];
+                if (sum == k)
                     output++;
             }
         }
